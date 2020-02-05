d53f2d6bd049f3763708a45584a3183b8f27a520
osluocra <osluocra@icloud.com>
2020-02-05 14:22:42 -0600

    Initial

---
+++ how c++ works/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "log.h"
+
+using namespace std;
+
+//it works with both declarations
+//void log(const char* message); 
+void log(const char*);
+
+
+int main(){
+    
+    log("Hello World!");
+    return 0;
+}

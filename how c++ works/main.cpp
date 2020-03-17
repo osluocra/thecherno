4af19e812a6a7aa342f382c55d6a7f985f8c2737
osluocra <osluocra@icloud.com>
2020-03-17 12:34:30 -0500

    updated files

--- how c++ works/main.cpp
+++ how c++ works/main.cpp
@@ -7,9 +7,22 @@ using namespace std;
 //void log(const char* message); 
 void log(const char*);
 
+void printSizeOf(){
+    int i =9;
+    long l =9l;
+    float f =8.4f;
+    double d = 8.5;
+
+
+    cout << " i size of: " << i << sizeof(i) << endl;
+    cout << " l size of: " << l << sizeof(l) << endl;
+    cout << " f size of: " << f << sizeof(f) << endl;
+    cout << " d size of: " << d << sizeof(d) << endl;
+}
 
 int main(){
     
-    log("Hello World!");
+    log("Hello World from main!");
+    printSizeOf();
     return 0;
 #include "closeBracket.cpp"
